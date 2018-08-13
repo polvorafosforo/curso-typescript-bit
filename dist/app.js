53305dd7131eb54fc2e053ba4c5e34524a6159dd
polvorafosforo <arthur.lara.negocios.01@gmail.com>
2018-08-13 09:51:59 -0300

    Seção 3, Aula 9 (Completo)

--- dist/app.js
+++ dist/app.js
@@ -12,3 +12,12 @@ var isEnoughToBeatMF = function (parsecs) {
 };
 var distance = 11;
 console.log("Is " + distance + " parsecs enough to beat Millenium Falcon? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5,1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
+console.log("inc (5,2) = " + inc(5, 2));
